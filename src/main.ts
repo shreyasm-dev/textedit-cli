d2fceae539c850a20d6b163254d187b44a7f14d2
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-07 15:41:04 -0600

    Write it I guess that's it

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
+#!/usr/bin/env node
+
 import sourceMapSupport from 'source-map-support';
+import { program } from 'commander';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { exec } from 'child_process';
 
 sourceMapSupport.install();
+
+const pkg = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf8'));
+
+program
+  .name(pkg.name)
+  .version(pkg.version)
+  .description(pkg.description);
+
+program
+  .arguments('<file>')
+  .action((file) => {
+    const filePath = resolve(process.cwd(), file);
+    exec(`open -a TextEdit ${filePath}`);
+  });
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+}
